2054259642d250057cdd3862a3aee85c47738d8e
Diego Alejandro <alejandroroseroc@gmail.com>
2024-11-23 14:57:32 -0500

    cambiando de fuente de letra

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ import Header from "@/components/header";
 const urbanist = Urbanist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "TarreDev Landing Page",
-  description: "Landing page made by TarreDev",
+  title: "Portafolio de Alejandro",
+  description: "Diego Alejandro Portafolio",
 };
 
 export default function RootLayout({
